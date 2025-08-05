2a6d185c2e90b7590d25fe1e178bdd449c094896
Bradlee Harrison <56418392+bradleeharr@users.noreply.github.com>
2025-08-05 11:53:53 -0500

    Update Mail.lua - Remove Deleting Items
    `mail` has a pretty critical flaw.
    
    It will attempt to delete mail that you open without any checks to see if you have actually received the loot from it.
    
    This can lead to deleted items if you or the server is lagging enough.
    
    https://www.reddit.com/r/turtlewow/comments/1gv6w6v/comment/ly0a5tr/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- Mail.lua
+++ Mail.lua
@@ -27,7 +27,7 @@ do
 	function GetCursorItem()
 		return cursorItem
 	end
-	function SetCursorItem(item)
+	function SetCursorItem(item)D
 		f:SetScript('OnUpdate', function()
 			cursorItem = item
 			f:SetScript('OnUpdate', nil)
@@ -225,7 +225,7 @@ do
 		GetInboxText(i)
 		TakeInboxMoney(i)
 		TakeInboxItem(i)
-		DeleteInboxItem(i)
+		--DeleteInboxItem(i)
 	end
 end
 
